39c947c76a3a58e60521f03c486fe32dcc764997
FlorFuentes <flopy_200412@hotmail.com>
2023-09-27 16:07:42 -0300

    Creacion de app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import Memes from './components/Memes.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Memes/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Memes.jsx
@@ -0,0 +1,56 @@
+import html2canvas from "html2canvas";
+import React, { useState } from "react";
+
+
+
+const Memes = () => {
+
+
+
+    const [imgmeme, setImgmeme] = useState();
+    const [textmeme, setTextmeme] = useState();
+
+    const textomeme = (e) => {
+        setTextmeme(e.target.value);
+    }
+
+    const selecionarImg = (e) => {
+        setTextmeme(e.target.value);
+    }
+
+    const descargar = (e) => {
+        html2canvas(document.querySelector("#exportar")).then(function (canvas) {
+            {/*document.body.appendChild(canvas);*/ }
+            let img = canvas.toDataURL("memes/jpg"); /*leer documentacion html2canvas*/
+            let link = document.createElement("a"); /*creo elemento para descarga*/
+            link.download = "memepropio.jpg";  /* apunto al elemento creado en el renglomn de arriba para descargar*/
+            link.href = img;
+            link.click();
+        });
+    }
+
+    return (
+        <div className="text-center">
+            <h1 className="mt-3 mb-3 text-center">Editor de memes</h1>
+            <h3>Ingresa el texto del meme</h3>
+            <input onChange={textomeme} className="form-control w-50 m-50 m-auto d-block" type="text" placeholder="" />
+
+            <h3 className="mt-3 mb-3 text-center">Elegi tu imagen favorita</h3>
+            <select onChange={selecionarImg} className="form-select form-select-lg mb-3 w-50 m-auto" arial-label=".form-select-lg example">
+                <option value={1}>Futurama</option>
+                <option value={2}>Bob esponja</option>
+                <option value={3}>Señora</option>
+                <option value={4}>Calamardo</option>
+            </select>
+
+            <figure className="text-center" id="exportar">
+                <p>{textmeme}</p>
+                <img src={`./img/${imgmeme}.png`} className="figure-img mt-3 d-block m-auto" alt="meme"/>
+            </figure>
+
+            <button onClick={descargar} type="button" className="btn btn-primary mt-4 mb-4">Descargar meme</button>
+
+        </div>
+    )
+}
+export default Memes; 
