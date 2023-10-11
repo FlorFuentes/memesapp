0f61c2048f77a879d28f14f0706b947aa2b0b591
FlorFuentes <flopy_200412@hotmail.com>
2023-10-11 14:50:51 -0300

    consumiendo Api

--- src/components/Memes.jsx
+++ src/components/Memes.jsx
@@ -1,10 +1,18 @@
 import html2canvas from "html2canvas";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Memes = () => {
 
-    const [imgmeme,setImgmeme]= useState();
-    const [textmeme, setTextmeme] = useState();
+    const [imgmeme,setImgmeme]= useState("");
+    const [textmeme, setTextmeme] = useState("");
+    const [memes, setMemes]= useState([]);
+
+
+    useEffect(()=>{
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(data=> setMemes(data.data.memes));
+    },[]);
 
     const textomeme = (e) => {
         setTextmeme(e.target.value);
@@ -15,32 +23,39 @@ const Memes = () => {
     }
 
     const descargar = (e) => {
+        const meme= memes.find(m=> m.name === imgmeme);
+        if(!meme)return;
+
+
+        
+
         html2canvas(document.querySelector("#exportar")).then(function (canvas) {
             /*document.body.appendChild(canvas);*/ 
-            let img = canvas.toDataURL("memes/jpg"); /*leer documentacion html2canvas*/
+            let img = canvas.toDataURL("image/jpg"); /*leer documentacion html2canvas*/
             let link = document.createElement("a"); /*creo elemento para descarga*/
             link.download = "memepropio.jpg";  /* apunto al elemento creado en el renglomn de arriba para descargar*/
             link.href = img;
             link.click();
         });
+
+        
     }
     return (
         <div className="text-center">
             <h1 className="mt-3 mb-3 text-center">Editor de memes</h1>
             <h3>Ingresa el texto del meme</h3>
-            <input onChange={textomeme} className="form-control w-50 m-50 m-auto d-block" type="text" placeholder="" />
+            <input onChange={textomeme} className="form-control w-50 m-auto d-block" type="text" placeholder="" />
 
-            <h3 className="mt-3 mb-3 text-center">Elegi tu imagen favorita</h3>
-            <select onChange={seleccionarImg} className="form-select form-select-lg mb-3 w-50 m-auto" aria-label=".form-select-lg example">
-                <option value={1}>Futurama</option>
-                <option value={2}>Bob esponja</option>
-                <option value={3}>Señora</option>
-                <option value={4}>Calamardo</option>
+            <h3 className="mt-3 mb-3 text-center">Elige tu meme favorito</h3>
+            <select onChange={seleccionarImg} value={imgmeme} className="form-select form-select-lg mb-3 w-50 m-auto" aria-label=".form-select-lg example">
+                {memes.map((meme, index) => (
+                    <option key={index} value={meme.name}>{meme.name}</option>
+                ))}
             </select>
 
             <figure className="text-center" id="exportar">
                 <p>{textmeme}</p>
-                <img src={`../img/${imgmeme}.jpg`} className="figure-img mt-3 d-block m-auto" alt="meme"/>
+                <img src={memes.find(m => m.name === imgmeme)?.url} className="figure-img mt-3 d-block m-auto" alt="meme" />
             </figure>
 
             <button onClick={descargar} type="button" className="btn btn-primary mt-4 mb-4">Descargar meme</button>
